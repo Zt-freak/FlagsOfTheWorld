6e6b7f8e79f291e1cb7d3a83ade3e5f60855b42b
Zt-freak <bnkho98@hotmail.com>
2020-12-19 22:14:41 +0100

    actually implemented the game

--- FlagsOfTheWorld/Form1.cs
+++ FlagsOfTheWorld/Form1.cs
@@ -18,9 +18,12 @@ namespace FlagsOfTheWorld
 {
     public partial class Form1 : Form
     {
-        public List<CountriesRoot> countries;
-        private const string URL = "https://restcountries.eu/rest/v2/all?fields=name;alpha2Code";
-        private const string DATA = @"";
+        private List<CountriesRoot> countries;
+        private CountriesRoot currentCountry;
+        private int gameMode = 0;
+        private int score = 0;
+        private const string countriesURL = "https://restcountries.eu/rest/v2/all?fields=name;alpha2Code";
+        private const string countriesData = @"";
         public Form1()
         {
             this.countries = this.Fetch_countries();
@@ -31,14 +34,15 @@ namespace FlagsOfTheWorld
         private void Form1_Load(object sender, EventArgs e)
         {
             this.Fill_dropdown(this.dropdown, this.countries);
+            this.switchModeButton.Text = "play";
         }
 
         private List<CountriesRoot> Fetch_countries()
         {
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(countriesURL);
             request.Method = "GET";
             request.ContentType = "application/json";
-            request.ContentLength = DATA.Length;
+            request.ContentLength = countriesData.Length;
 
             try
             {
@@ -64,6 +68,66 @@ namespace FlagsOfTheWorld
             {
                 dropdown.Items.Add(country.name);
             }
+        }
+
+        private void switchModeButton_Click(object sender, EventArgs e)
+        {
+            switch (this.gameMode)
+            {
+                case 0: // practice mode to game mode
+                    this.new_round();
+                    this.gameMode = 1;
+                    this.switchModeButton.Text = "practice";
+                    break;
+                case 1: // game mode to practice mode
+                    this.gameMode = 0;
+                    this.switchModeButton.Text = "play";
+                    break;
+            }
+        }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            CountriesRoot country = this.countries.FirstOrDefault(item => item.name == this.dropdown.Text);
+            string flagURL;
+            switch (this.gameMode)
+            {
+                case 0: // practice mode
+                    if (country != null)
+                    {
+                        flagURL = "https://www.countryflags.io/" + country.alpha2Code + "/flat/64.png";
+                    }
+                    else
+                    {
+                        flagURL = "https://upload.wikimedia.org/wikipedia/commons/c/cc/Flag_missing.jpg";
+                    }
+                    this.flagBox.Load(flagURL);
+                    break;
+                case 1: // game mode
+                    if (country != null)
+                    {
+                        if (country.alpha2Code == this.currentCountry.alpha2Code)
+                        {
+                            this.score++;
+                        }
+                        else
+                        {
+                            this.score = 0;
+                        }
+                        this.scoreLabel.Text = "score: " + this.score.ToString();
+                        this.new_round();
+                    }
+                    break;
+            }
+        }
+
+        private void new_round()
+        {
+            var random = new Random();
+            int randomIndex = random.Next(this.countries.Count);
+            this.currentCountry = this.countries[randomIndex];
+            string flagURL = "https://www.countryflags.io/" + this.currentCountry.alpha2Code + "/flat/64.png";
+            this.flagBox.Load(flagURL);
         }
     }
 
